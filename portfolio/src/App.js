56b83494085cd377d0a614823f0c02d3207bc875
MariuszWiacek <wiacekm.uber@gmail.com>
2023-03-01 07:38:49 +0000

    react added and bootstrap

---
+++ portfolio/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HelloBootstrap from "./components/HelloBootstrap"
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+function App() {
+  return <HelloBootstrap />;
+}
+
+export default App;
