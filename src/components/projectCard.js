53d3ce8e019284553938c2472e493b7fd32c952f
MariuszWiacek <wiacekm.uber@gmail.com>
2023-03-08 20:27:47 +0000

    buttons css added to portfolio gallery

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -11,22 +11,22 @@ function ProjectCard(props) {
       </div>
       <div className="content">
         <ul>
-        <li style={{ textAlign: 'center', fontSize: '40px', borderBottom: "white"}}>
+        <li style={{ textAlign: 'left', fontSize: '40px', borderBottom: "white"}}>
             {props.name}
           </li>
-          <li style={{ textAlign: 'center', fontSize: '15px'}}>
+          <li style={{ textAlign: 'left', fontSize: '15px'}}>
            {props.technologies}
           </li>
-          <li style={{ textAlign: 'center', fontSize: '15px'}}>
+          <li style={{ textAlign: 'left', fontSize: '15px'}}>
            {props.description}
           </li>
-          <li style={{ textAlign: 'center', fontSize: '20px'}}>
+          <li style={{ textAlign: 'left', fontSize: '20px'}}>
 
-            <a style={{textDecoration: 'none'}} href={props.deployed}>Deployed project</a>
+            <button class="button-53" style={{color:"red", fontSize:"20px", border: '1px solid white'}} href={props.deployed}>Click to check this project</button>
 
           </li>
-          <li style={{ textAlign: 'center', fontSize: '20px', textDecoration: 'none'}}>
-            <a style={{textDecoration: 'none'}} href={props.github}>GitHub</a>
+          <li style={{ textAlign: 'left', fontSize: '20px', textDecoration: 'none'}}>
+            <button class="button-53" style={{color:"red", fontSize:"20px", border: '1px solid white'}} href={props.github}>GitHub Respository</button>
 
           </li>
         </ul>
