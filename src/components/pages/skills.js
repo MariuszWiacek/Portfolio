8514d425d9d7313e422270fc5013a097e4f5062b
MariuszWiacek <wiacekm.uber@gmail.com>
2023-03-11 16:16:58 +0000

    images

--- src/components/pages/skills.js
+++ src/components/pages/skills.js
@@ -7,13 +7,12 @@ const Skills = () => {
       <h2 style={{ fontSize: "50px" }}>Skills</h2>
       
       <ul class="centered" style={{ paddingBottom: "3%" }}>
-        <img style={{ height:"150px", width:"150px", paddingRight:"5px" }} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="The logo icon for react" title="React"></img>
-        <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="The logo icon for HTML 5" title="HTML 5"></img >
-        <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="The logo icon for CSS3" title="CSS 3"></img >
-        <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="The logo icon for TailwindCSS" title="Twilwind CSS"></img >
-      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="The logo icon for GitHub" title="GitHub"></img >
-      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="The logo icon for NPM" title="NPM"></img >
-      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="https:/cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="The logo icon for NodeJS" title="Node JS"></img >
+        <img style={{ height:"150px", width:"150px", paddingRight:"5px" }} src="../images/react.jpg" alt="The logo icon for react" title="React"></img>
+        <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="../images/react.jpg" alt="The logo icon for CSS3" title="CSS 3"></img>
+        <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="../images/react.jpg" alt="The logo icon for TailwindCSS" title="Twilwind CSS"></img>
+      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="../images/react.jpg" alt="The logo icon for GitHub" title="GitHub"></img>
+      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="../images/react.jpg" alt="The logo icon for NPM" title="NPM"></img>
+      <img style={{ height:"150px", width:"150px", paddingRight:"5px"}} src="../images/react.jpg" alt="The logo icon for NodeJS" title="Node JS"></img>
 
     </ul>
     <p>Creating web and mobile applications <br />
