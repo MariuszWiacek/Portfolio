339588119d122d39cb1c09376d3e548d9fa82488
MariuszWiacek <wiacek.mariusz@gmail.com>
2023-04-08 13:10:56 +0100

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,13 @@ function Footer() {
   return (
     <footer style={{ width: "100%", marginRight : "5%",  display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
   <div style={{ flex: 1 }}>
-    <span>® 2023 Mariusz Wiacek  </span>
+    <span>wiacek.mariusz@gmail.com </span>
   </div>
   <div style={{ flex: 1, textAlign: 'center' }}>
-    <span style={{color:"orange"}}> www.mariuszwiacek.com</span>
+    <span> www.mariuszwiacek.com</span>
   </div>
   <div style={{ flex: 1, textAlign: 'right' }}>
-    <span style={{marginRight : "3%"}}> wiacek.mariusz@gmail.com</span>
+    <span style={{marginRight : "3%"}}>© 2023 Mariusz-Portfolio</span>
   </div>
 </footer>)
 }
