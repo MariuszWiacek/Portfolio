0bc0902dfcb86631eea640a781d1dadf02c8a07d
MariuszWiacek <wiacek.mariusz@gmail.com>
2023-04-08 12:57:17 +0100

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,20 @@
 
 function Footer() {
   return (
-    <footer>® 2023 Mariusz Wiacek
-       email : wiacek.mariusz@gmail.com
-      
-    
-      </footer>)
+    <footer style={{ width: "100%", marginRight : "5%",  display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  <div style={{ flex: 1 }}>
+    <span>® 2023 Mariusz Wiacek  </span>
+  </div>
+  <div style={{ flex: 1, textAlign: 'center' }}>
+    <span style={{color:"orange"}}> www.mariuszwiacek.com</span>
+  </div>
+  <div style={{ flex: 1, textAlign: 'right' }}>
+    <span style={{marginRight : "3%"}}> wiacek.mariusz@gmail.com</span>
+  </div>
+</footer>)
 }
+
+
 
 
 export default Footer;
