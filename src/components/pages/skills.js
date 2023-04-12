dd34f63d69128a64a702fd829866297fd7ead493
MariuszWiacek <wiacek.mariusz@gmail.com>
2023-04-12 21:31:42 +0100

    skills updated

--- src/components/pages/skills.js
+++ src/components/pages/skills.js
@@ -9,12 +9,13 @@ const Skills = () => {
     <h1 style={{fontSize: '60px', marginTop: '5%', }}>Skills</h1>
     <div>
   <p style={{width:"75%", height: "400px", overflowY: 'auto'}}>
-  <span style={{ color: 'orange' , fontWeight: "700"}}>HTML/CSS: </span>Proficient in creating well-structured and visually appealing web pages using HTML and CSS.<br /><br />
+  <span style={{ color: 'orange' , fontWeight: "700"}}>HTML5 /CSS : </span>Proficient in creating well-structured and visually appealing web pages using HTML and CSS.<br /><br />
 
-  <span style={{ color: 'orange', fontWeight: "700" }}>JavaScript:</span> Proficient in JavaScript, including frameworks such as React, Angular, and Vue, to create dynamic and interactive web applications.<br /><br />
-
+  <span style={{ color: 'orange', fontWeight: "700" }}>JavaScript:</span> Proficient in JavaScript, including React to create dynamic and interactive web applications.<br /><br />
+  <span style={{ color: 'orange', fontWeight: "700" }}>Node.js :</span>  I am skilled in using Node.js to develop server-side applications<br /><br />
+  <span style={{ color: 'orange', fontWeight: "700" }}>APIs :</span>  I have nowledge how to use APIs to integrate different data, applications and services.<br /><br />
   <span style={{ color: 'orange', fontWeight: "700" }}>Responsive Design:</span>  Skilled in creating responsive designs that work across different devices and screen sizes to ensure a seamless user experience.<br /><br />
-
+  
     <span style={{ color: 'orange', fontWeight: "700" }}> User Experience (UX):</span> Understanding of user experience principles and skilled in designing websites that are easy to use and navigate, providing users with an enjoyable experience.<br /><br />
 
     <span style={{ color: 'orange', fontWeight: "700" }}>Accessibility:</span> Knowledgeable in accessibility guidelines such as WCAG 2.1 and skilled in creating websites that are accessible to everyone, including those with disabilities, to ensure inclusivity.<br /><br />
@@ -23,9 +24,9 @@ const Skills = () => {
 
     <span style={{ color: 'orange' , fontWeight: "700"}}>Performance Optimization: </span>Skilled in optimizing website performance, including skills such as minification, image optimization, and lazy loading, to create fast-loading websites that provide a smooth user experience.<br /><br />
 
-    <span style={{ color: 'orange' , fontWeight: "700"}}>Version Control: </span>Proficient in Git and version control, allowing for collaboration with other developers and contributions to larger projects.<br /><br />
+    <span style={{ color: 'orange' , fontWeight: "700"}}>Version Control: </span>Proficient in Github and version control, allowing for collaboration with other developers and contributions to larger projects.<br /><br />
 
-    <span style={{ color: 'orange', fontWeight: "700" }}>Debugging and Troubleshooting: </span>Skilled in troubleshooting and debugging code, demonstrating strong problem-solving abilities.<br /><br />
+    <span style={{ color: 'orange', fontWeight: "700" }}>Debugging and Troubleshooting: </span>Jest / Enzyme - Skilled in troubleshooting and debugging code, demonstrating strong problem-solving abilities.<br /><br />
 
     <span style={{ color: 'orange', fontWeight: "700" }}>Communication: </span>Strong communication skills, with the ability to articulate technical concepts to both technical and non-technical stakeholders.<br /><br />
 
