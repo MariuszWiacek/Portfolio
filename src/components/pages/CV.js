c5298f62840592fb34090ab70e87249ed6bc8143
Mariusz Wiacek <wiacekm.uber@gmail.com>
2023-09-14 20:50:51 +0100

    cv

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -8,9 +8,7 @@ const CV = () => {
       <div >
         <h1 style={{ fontSize: '60px', marginTop: '5%' }}>CV </h1>
    <p>My professional Resume : </p>
-        <a href="https://docs.google.com/document/d/e/2PACX-1vSvtIlj1dXXmALJ23IMS9chq5NbAiVIP5PRKx8s3q88eN-jqEqEnlXcnJbiHQHybA/pub" download>
-          <button id="cv" class="bi bi-file-earmark-person"></button>
-        </a>
+   <iframe src="https://docs.google.com/document/d/e/2PACX-1vSvtIlj1dXXmALJ23IMS9chq5NbAiVIP5PRKx8s3q88eN-jqEqEnlXcnJbiHQHybA/pub?embedded=true"></iframe>
         <p style={{ overflowY: 'auto', width: "30%"}}>
 
         You can click on the image above to view my full CV, which provides a comprehensive overview of my education, work experience, skills, achievements, and other relevant information. Thank you for your interest!
