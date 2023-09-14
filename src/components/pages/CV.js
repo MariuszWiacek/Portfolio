f1064e006de82c13e7ea5da177b6feb93cb6eca5
Mariusz Wiacek <wiacekm.uber@gmail.com>
2023-09-14 20:50:06 +0100

    cv

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -8,7 +8,7 @@ const CV = () => {
       <div >
         <h1 style={{ fontSize: '60px', marginTop: '5%' }}>CV </h1>
    <p>My professional Resume : </p>
-        <a href="https://docs.google.com/document/d/1v7ricqiVdlesBmrzpkOX8PomJaN1xn3_/edit?usp=sharing&ouid=106269992883549290468&rtpof=true&sd=true" download>
+        <a href="https://docs.google.com/document/d/e/2PACX-1vSvtIlj1dXXmALJ23IMS9chq5NbAiVIP5PRKx8s3q88eN-jqEqEnlXcnJbiHQHybA/pub" download>
           <button id="cv" class="bi bi-file-earmark-person"></button>
         </a>
         <p style={{ overflowY: 'auto', width: "30%"}}>
